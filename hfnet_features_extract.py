0c8f3ceac7a23a135f00f4ffeebad24254bb2e6e
Dong Liu <dliu.cn@gmail.com>
2021-04-03 14:42:41 -0400

    make them import able

--- hfnet_features_extract.py
+++ hfnet_features_extract.py
@@ -9,38 +9,6 @@ import tensorflow as tf
 from tensorflow.python.saved_model import tag_constants
 tf.contrib.resampler  # import C++ op
 
-from argparse import ArgumentParser
-
-parser = ArgumentParser(description='Features Extract')
-parser.add_argument(
-    '-image-dir', '--image-dir',
-    type=Path,
-    help='image dir'
-)
-
-parser.add_argument(
-    '-output', '--output',
-    type=Path, default='output',
-    help='Output folder for featues'
-)
-
-parser.add_argument(
-    '-gen-local', '--gen-local',
-      action='store_true',
-      help='Generate local feature'
-)
-
-parser.add_argument(
-    '-nms_radius', '--nms_radius',
-      type=int, default=4
-)
-parser.add_argument(
-    '-keypoints', '--keypoints',
-      type=int, default=4096
-)
-
-args=parser.parse_args()
-
 class HFNet:
     def __init__(self, model_path, outputs):
         self.session = tf.Session()
@@ -65,46 +33,84 @@ class HFNet:
         }
         return self.session.run(self.outputs, feed_dict=inputs)
 
-model_path = Path(EXPER_PATH, 'saved_models/hfnet')
-outputs = ['global_descriptor', 'keypoints', 'local_descriptors', 'scores']
-hfnet = HFNet(model_path, outputs)
+def get_hfnet() :
+    model_path = Path(EXPER_PATH, 'saved_models/hfnet')
+    outputs = ['global_descriptor', 'keypoints', 'local_descriptors', 'scores']
+    return HFNet(model_path, outputs)
 
-globs=['*.jpg', '*.png', '*.jpeg', '*.JPG', '*.PNG']
-image_paths = []
-for g in globs:
-    image_paths += list(Path(args.image_dir).glob('**/'+g))
-if len(image_paths) == 0:
-    raise ValueError(f'Could not find any image in root: {root}.')
-image_paths = sorted(list(set(image_paths)))
-image_paths = [i.relative_to(args.image_dir) for i in image_paths]
-#print('image_paths:{}]'.format(image_paths))
+def main(image_dir, output_dir, gen_local=True, nms_radius=4, keypoints=4096):
+    globs=['*.jpg', '*.png', '*.jpeg', '*.JPG', '*.PNG']
+    image_paths = []
+    for g in globs:
+        image_paths += list(Path(image_dir).glob('**/'+g))
+    if len(image_paths) == 0:
+        raise ValueError(f'Could not find any image in root: {root}.')
+    image_paths = sorted(list(set(image_paths)))
+    image_paths = [i.relative_to(image_dir) for i in image_paths]
+    #print('image_paths:{}]'.format(image_paths))
 
-global_feature_path=Path(args.output, 'global_features.h5')
-global_feature_path.parent.mkdir(exist_ok=True, parents=True)
-global_feature_file = h5py.File(str(global_feature_path), 'w')
-if args.gen_local:
-      local_feature_path=Path(args.output, 'feats-superpoint.h5')
-      local_feature_file = h5py.File(str(local_feature_path), 'w')
+    global_feature_path=Path(output_dir, 'global_features.h5')
+    global_feature_path.parent.mkdir(exist_ok=True, parents=True)
+    global_feature_file = h5py.File(str(global_feature_path), 'w')
+    if gen_local:
+          local_feature_path=Path(output_dir, 'feats-superpoint.h5')
+          local_feature_file = h5py.File(str(local_feature_path), 'w')
 
-mode = cv2.IMREAD_COLOR
-cnt = 0
-for im_file in image_paths:
-    image = cv2.imread(str(args.image_dir / im_file), mode)
-    db = hfnet.inference(image, nms_radius=args.nms_radius, num_keypoints=args.keypoints)
-    grp = global_feature_file.create_group(str(im_file))
-    grp.create_dataset('global_descriptor', data=db['global_descriptor'])
-    if args.gen_local:
-        grp = local_feature_file.create_group(str(im_file))
-        grp.create_dataset('keypoints', data=db['keypoints'].astype(np.float32))
-        grp.create_dataset('descriptors', data=db['local_descriptors'].T)
-        grp.create_dataset('scores', data=db['scores'])
-        size = image.shape[:2][::-1]
-        grp.create_dataset('image_size', data=np.array(size))
-    cnt += 1
-    if cnt % 200 == 0 :
-        print('{} images processed'.format(cnt))
+    hfnet = get_hfnet()
+    mode = cv2.IMREAD_COLOR
+    cnt = 0
+    for im_file in image_paths:
+        image = cv2.imread(str(image_dir / im_file), mode)
+        db = hfnet.inference(image, nms_radius=nms_radius, num_keypoints=keypoints)
+        grp = global_feature_file.create_group(str(im_file))
+        grp.create_dataset('global_descriptor', data=db['global_descriptor'])
+        if gen_local:
+            grp = local_feature_file.create_group(str(im_file))
+            grp.create_dataset('keypoints', data=db['keypoints'].astype(np.float32))
+            grp.create_dataset('descriptors', data=db['local_descriptors'].T)
+            grp.create_dataset('scores', data=db['scores'])
+            size = image.shape[:2][::-1]
+            grp.create_dataset('image_size', data=np.array(size))
+        cnt += 1
+        if cnt % 200 == 0 :
+            print('{} images processed'.format(cnt))
 
-global_feature_file.close()
-if args.gen_local:
-    local_feature_file.close()
-print('Finished exporting features.')
+    global_feature_file.close()
+    if gen_local:
+        local_feature_file.close()
+    print('Finished exporting features.')
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description='Features Extract')
+    parser.add_argument(
+        '-image-dir', '--image-dir',
+        type=Path,
+        help='image dir'
+    )
+
+    parser.add_argument(
+        '-output', '--output',
+        type=Path, default='output',
+        help='Output folder for featues'
+    )
+
+    parser.add_argument(
+        '-gen-local', '--gen-local',
+          action='store_true',
+          help='Generate local feature'
+    )
+
+    parser.add_argument(
+        '-nms_radius', '--nms_radius',
+          type=int, default=4
+    )
+    parser.add_argument(
+        '-keypoints', '--keypoints',
+          type=int, default=4096
+    )
+
+    args=parser.parse_args()
+
+    main(args.image_dir, args.output, gen_local=args.gen_local, nms_radius=args.nms_radius, keypoints=args.keypoints)
--- netvlad_feature_extract.py
+++ netvlad_feature_extract.py
@@ -10,57 +10,61 @@ import tensorflow as tf
 from tensorflow.python.saved_model import tag_constants
 tf.contrib.resampler  # import C++ op
 
-from argparse import ArgumentParser
+def get_netvlad() :
+    checkpoint_path = Path(EXPER_PATH, 'vd16_pitts30k_conv5_3_vlad_preL2_intra_white/vd16_pitts30k_conv5_3_vlad_preL2_intra_white')
+    config = {'checkpoint_path':checkpoint_path, 'data': {'name': 'aachen', 'load_db': False, 'load_queries': True, 'resize_max': 960}, 'model': {'name': 'netvlad_original', 'local_descriptor_layer': 'conv3_3', 'image_channels': 1}, 'weights': 'vd16_pitts30k_conv5_3_vlad_preL2_intra_white/vd16_pitts30k_conv5_3_vlad_preL2_intra_white'}
 
-parser = ArgumentParser(description='Features Extract')
-parser.add_argument(
-    '-image-dir', '--image-dir',
-    type=Path,
-    help='image dir'
-)
+    net = get_model(config['model']['name'])(data_shape={'image': [None, None, None, 3]},**config['model'])
+    net.load(str(checkpoint_path))
+    return net
 
-parser.add_argument(
-    '-output', '--output',
-    type=Path, default='output',
-    help='Output folder for featues'
-)
+def main(image_dir, output_dir):
+    globs=['*.jpg', '*.png', '*.jpeg', '*.JPG', '*.PNG']
+    image_paths = []
+    for g in globs:
+        image_paths += list(Path(image_dir).glob('**/'+g))
+    if len(image_paths) == 0:
+        raise ValueError(f'Could not find any image in root: {image_dir}.')
+    image_paths = sorted(list(set(image_paths)))
+    image_paths = [i.relative_to(image_dir) for i in image_paths]
+    #print('image_paths:{}]'.format(image_paths))
 
-args=parser.parse_args()
+    global_feature_path=Path(output_dir, 'global_features.h5')
+    global_feature_path.parent.mkdir(exist_ok=True, parents=True)
+    global_feature_file = h5py.File(str(global_feature_path), 'w')
 
-checkpoint_path = Path(EXPER_PATH, 'vd16_pitts30k_conv5_3_vlad_preL2_intra_white/vd16_pitts30k_conv5_3_vlad_preL2_intra_white')
-config = {'checkpoint_path':checkpoint_path, 'data': {'name': 'aachen', 'load_db': False, 'load_queries': True, 'resize_max': 960}, 'model': {'name': 'netvlad_original', 'local_descriptor_layer': 'conv3_3', 'image_channels': 1}, 'weights': 'vd16_pitts30k_conv5_3_vlad_preL2_intra_white/vd16_pitts30k_conv5_3_vlad_preL2_intra_white'}
+    net = get_netvlad()
 
-globs=['*.jpg', '*.png', '*.jpeg', '*.JPG', '*.PNG']
-image_paths = []
-for g in globs:
-    image_paths += list(Path(args.image_dir).glob('**/'+g))
-if len(image_paths) == 0:
-    raise ValueError(f'Could not find any image in root: {args.image_dir}.')
-image_paths = sorted(list(set(image_paths)))
-image_paths = [i.relative_to(args.image_dir) for i in image_paths]
-#print('image_paths:{}]'.format(image_paths))
+    keys = ['global_descriptor']
+    cnt = 0
+    for im_file in image_paths:
+        image = cv2.imread(str(image_dir / im_file))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        predictions = net.predict({'image':image}, keys=keys)
+        grp = global_feature_file.create_group(str(im_file))
+        grp.create_dataset('global_descriptor', data=predictions['global_descriptor'])
+        cnt += 1
+        if cnt % 200 == 0 :
+            print('{} images processed'.format(cnt))
 
-global_feature_path=Path(args.output, 'global_features.h5')
-global_feature_path.parent.mkdir(exist_ok=True, parents=True)
-global_feature_file = h5py.File(str(global_feature_path), 'w')
+    global_feature_file.close()
+    print('Finished exporting features.')
 
-with get_model(config['model']['name'])(
-            data_shape={'image': [None, None, None, 3]},
-            **config['model']) as net:
-      if checkpoint_path is not None:
-            net.load(str(checkpoint_path))
+if __name__ == '__main__':
+    from argparse import ArgumentParser
 
-      keys = ['global_descriptor']
-      cnt = 0
-      for im_file in image_paths:
-            image = cv2.imread(str(args.image_dir / im_file))
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            predictions = net.predict({'image':image}, keys=keys)
-            grp = global_feature_file.create_group(str(im_file))
-            grp.create_dataset('global_descriptor', data=predictions['global_descriptor'])
-            cnt += 1
-            if cnt % 200 == 0 :
-                  print('{} images processed'.format(cnt))
+    parser = ArgumentParser(description='Features Extract')
+    parser.add_argument(
+        '-image-dir', '--image-dir',
+        type=Path,
+        help='image dir'
+    )
 
-global_feature_file.close()
-print('Finished exporting features.')
+    parser.add_argument(
+        '-output', '--output',
+        type=Path, default='output',
+        help='Output folder for featues'
+    )
+
+    args=parser.parse_args()
+    main(args.image_dir, args.output)
